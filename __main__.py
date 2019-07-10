0fa3ce3719926d1804986ea621153061092966d2
Andrew Engleman <drew9781@yahoo.com>
2019-07-09 20:38:29 -0400

    feat: parse proxmox resources for IP/hostnames into json

--- __main__.py
+++ __main__.py
@@ -1,16 +1,41 @@
 #! /usr/bin/python3
 import json
 import sys
+import re
 
 def main():
-    file = sys.argv[1]
-    parse(file)
+   file = sys.argv[1]
+   with open(file, 'r') as myFile:
+      #buffer = (myFile.read())
+      resources = json.load(myFile)
+      resources = resources['values']['root_module']['resources']
+   
+   inventory = []
+   for key in resources:
+      if key['provider_name'] == 'proxmox':
+         buff = parseResourceProxmox( key )
+         inventory.append(buff)
+      #print(key)
+      
+   
+   print (inventory)
+
+def parseResourceProxmox( _resource ):
+   inventory = {}
+   name      = _resource['name']
+   _resource = _resource['values']
+   
+   inventory[ name ] =  {}
+   inventory[ name ][ 'ip' ]   =   parseProxmoxIP( _resource['ipconfig0'] )
+   inventory[ name ][ 'vm' ]   =   _resource['name']  
+   #inventory[ name ][ 'tags' ] =   _resource['tags']  - feat request
 
 
-def parse( file ):
-     with open(file, 'r') as myFile:
-        #buffer = (myFile.read())
-        buffer = json.load(myFile)
-     print(buffer['values']['root_module']['resources'][0]['name'])
+   return inventory
+
+def parseProxmoxIP( preFormat ):
+   ip = re.findall( r'[0-9]+(?:\.[0-9]+){3}', preFormat )
+   return ip[0]
+
 
 main()
